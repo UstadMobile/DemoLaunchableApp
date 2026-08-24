e2f0ab5972f1edd01d30c4ae197a0a32e109e889
Mike Dawson <mike@ustadmobile.com>
2026-08-24 18:04:33 +0400

    Change package names for http-ipc.
    HTTP-IPC is not really limited to caching: remove the word cache from the module names.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ kotlin {
 dependencies {
     implementation(libs.openeel.xapi.core)
     implementation(libs.openeel.xapi.ipc.client)
-    implementation(libs.openeel.lib.cache.ipc.client)
+    implementation(libs.openeel.lib.http.ipc.client)
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
